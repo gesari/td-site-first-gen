5d93c2ac0a4551b1fda567b57101267b82f9b4c2
gesari <sirus1257@gmail.com>
2025-11-04 17:17:46 -0500

    added base dropdown implementation via menu.js, added in preliminary styling and tested on Degrees of Success page

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,31 @@
-// Hamburger dropdown transform function
-function menuTransform(x) {
-    x.classList.toggle("change");
+// Toggle dropdown menu visibility
+const toggleDropdown = (dropdown, menu, isOpen) => {
+    dropdown.classList.toggle("open", isOpen);
+    menu.style.height = isOpen ? `${menu.scrollHeight}px` : 0;
 }
+
+// Close all dropdowns
+const closeAllDropdowns = () => {
+    document.querySelectorAll(".dropdownContainer.open").forEach(openDropdown => {
+        toggleDropdown(openDropdown, openDropdown.querySelector(".dropdownMenu"), false);
+    })
+}
+
+// Attach click event to all dropdown toggles
+document.querySelectorAll(".dropdownToggle").forEach(dropdownToggle => {
+    dropdownToggle.addEventListener("click", e => {
+        e.preventDefault();
+
+        const dropdown = e.target.closest(".dropdownContainer");
+        const menu = dropdown.querySelector(".dropdownMenu");
+        const isOpen = dropdown.classList.contains("open");
+
+        closeAllDropdowns();
+
+        toggleDropdown(dropdown, menu, !isOpen);
+    });
+});
+
+document.querySelector(".dropdownContainer").addEventListener("click", () => {
+    document.querySelector(".dropdownMenu").classList.toggle("open");
+})
